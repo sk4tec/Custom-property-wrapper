ca2cb0bd0b91fcd3987cad98bf1fb6f05220e99f
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-29 08:16:15 +0100

    Initial Commit

---
+++ Custom property wrapper/Custom_property_wrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_property_wrapperApp.swift
+//  Custom property wrapper
+//
+//  Created by Sunjay Kalsi on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_property_wrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
