f14f934458487c96c6fed04f489829d4464219aa
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-06-29 08:18:11 +0100

    Custom property wrapper
    this example trims a url string

--- Custom property wrapper/ContentView.swift
+++ Custom property wrapper/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+import SwiftUI
+
+@propertyWrapper
+struct Trimmed {
+    var string: String
+
+    init(wrappedValue: String) {
+        self.string = wrappedValue
+    }
+
+    var wrappedValue: String {
+        get {
+            string.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        set {
+            string = newValue
+        }
+    }
+}
+
+struct API {
+    @Trimmed var url: String
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundColor(.accentColor)
+            Text(makeURL().absoluteString)
+                .font(.headline)
+                .background(Color.blue)
+                .padding()
         }
-        .padding()
+    }
+
+    func makeURL() -> URL {
+        var api = API(url: "https://myapp.com/  ")
+        return URL(string: api.url)!
     }
 }
 
